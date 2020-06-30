65c84f189d8ad7816684b0ce505ce44090f7924d
MarinaCarbonell <marinacarbonellalvarez@gmail.com>
2020-06-30 09:43:59 +0200

    clues implemented

--- main.js
+++ main.js
@@ -3,18 +3,33 @@
 //SELECTORS
 
 const fillNumber = document.querySelector ('.js-input-number');
-console.log (1);
-const testButton = document.querySelector ('js-test');
-console.log (2);
+const testButton = document.querySelector ('.js-test');
 const clue = document.querySelector ('.js-clue');
-console.log (3);
 const attemps = document.querySelector ('.js-attemps');
-console.log (4);
 
 //FUNCTIONS
 
-function functionName () {}
+function getRandomNumber (max) {
+  return Math.ceil (Math.random () * max);
+}
+
+const randomNumber = getRandomNumber (100);
+console.log (randomNumber);
+
+function pushTestButton () {
+  const inputNumber = fillNumber.value;
+  console.log (inputNumber);
+  if (inputNumber < randomNumber && inputNumber > 0) {
+    clue.innerHTML = 'Demasiado bajo';
+  } else if (inputNumber > randomNumber && inputNumber < 100) {
+    clue.innerHTML = 'Demasiado alto';
+  } else if (inputNumber < 0 || inputNumber > 100) {
+    clue.innerHTML = 'El número debe estar entre 1 y 100.';
+  } else {
+    clue.innerHTML = 'Has ganado campeona!!!';
+  }
+}
 
 //LISTENERS
 
-testButton.addEventListener ('click', functionName);
+testButton.addEventListener ('click', pushTestButton);
