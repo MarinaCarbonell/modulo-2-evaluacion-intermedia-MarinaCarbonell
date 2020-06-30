1745c44cfee728356c1ecc2d45be79d5dae26a2f
MarinaCarbonell <marinacarbonellalvarez@gmail.com>
2020-06-30 09:03:31 +0200

    add basic js structure

---
+++ main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+//SELECTORS
+
+const fillNumber = document.querySelector ('.js-input-number');
+console.log (1);
+const testButton = document.querySelector ('js-test');
+console.log (2);
+const clue = document.querySelector ('.js-clue');
+console.log (3);
+const attemps = document.querySelector ('.js-attemps');
+console.log (4);
+
+//FUNCTIONS
+
+function functionName () {}
+
+//LISTENERS
+
+testButton.addEventListener ('click', functionName);
